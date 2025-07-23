3d9482f1ab629b585c6862f938d817e8a3887ff6
MKWcode <michelle.woo588@gmail.com>
2025-07-22 23:35:28 -0400

    feat:created ContactsFragment

---
+++ app/src/main/java/com/b07safetyplanapp/ContactsFragment.java
@@ -0,0 +1,19 @@
+package com.b07safetyplanapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ContactsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_contact, container, false);
+        return view;
+    }
+}
