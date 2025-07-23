9be80947afa88b0650e171933de70014f5456e84
MKWcode <michelle.woo588@gmail.com>
2025-07-22 23:37:29 -0400

    feat:created SafeLocationsFragment

---
+++ app/src/main/java/com/b07safetyplanapp/SafeLocationsFragment.java
@@ -0,0 +1,19 @@
+package com.b07safetyplanapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SafeLocationsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_safe_location, container, false);
+        return view;
+    }
+}
