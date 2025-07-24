25e037e621d0fbe7afe51ad91b9370d3651a2708
MKWcode <michelle.woo588@gmail.com>
2025-07-24 16:15:07 -0400

    feat: added addItem and deleteItem methods

--- app/src/main/java/com/b07safetyplanapp/MedicationsFragment.java
+++ app/src/main/java/com/b07safetyplanapp/MedicationsFragment.java
@@ -4,16 +4,130 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MedicationsFragment extends Fragment {
+
+    private EditText editTextName, editTextDosage;
+
+    private Button buttonAdd;
+    private Button buttonEdit;
+    private Button buttonDelete;
+
+    private FirebaseDatabase db;
+    private DatabaseReference itemsRef;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_medication, container, false);
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextDosage = view.findViewById(R.id.editTextDosage);
+
+        buttonAdd = view.findViewById(R.id.buttonAdd);
+        buttonEdit = view.findViewById(R.id.buttonEdit);
+        buttonDelete = view.findViewById(R.id.buttonDelete);
+
+        db = FirebaseDatabase.getInstance("https://group8cscb07app-default-rtdb.firebaseio.com/");
+        itemsRef = db.getReference("users/userId123/safe_locations");
+
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteItem();
+            }
+        });
         return view;
     }
+
+    private void addItem() {
+        String name = editTextName.getText().toString().trim();
+        String dosage = editTextDosage.getText().toString().trim();
+
+
+        if (name.isEmpty() || dosage.isEmpty() ) {
+            Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //String id = itemsRef.push().getKey();
+        Medication item = new Medication(name, dosage);
+
+        itemsRef.child("medication").child(name).setValue(item).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void deleteItem() {
+        String name = editTextName.getText().toString().trim();
+        String dosage = editTextDosage.getText().toString().trim();
+
+
+        if (name.isEmpty() || dosage.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        itemsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean itemFound = false;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    SafeLocation item = snapshot.getValue(SafeLocation.class);
+                    if (item != null && item.getName().equalsIgnoreCase(name) && item.getName().equalsIgnoreCase(dosage)) {
+                        snapshot.getRef().removeValue().addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getContext(), "Item deleted", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getContext(), "Failed to delete item", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        itemFound = true;
+                        break;
+                    }
+                }
+                if (!itemFound) {
+                    Toast.makeText(getContext(), "Item not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
 }
