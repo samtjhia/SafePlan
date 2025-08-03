04d805273c617ffe7d7d21e004f85538a74bb8aa
MKWcode <michelle.woo588@gmail.com>
2025-08-03 17:40:53 -0400

    feat: added SafeLocation class to folder

--- app/src/main/java/com/b07safetyplanapp/models/emergencyinfo/SafeLocation.java
+++ app/src/main/java/com/b07safetyplanapp/models/emergencyinfo/SafeLocation.java
@@ -1,4 +1,4 @@
-package com.b07safetyplanapp;
+package com.b07safetyplanapp.models.emergencyinfo;
 
 public class SafeLocation {
     //private String id;
@@ -6,13 +6,16 @@ public class SafeLocation {
     private String address;
     private String notes;
 
+    private String id;
+
     SafeLocation(){
     }
-    SafeLocation( String name, String address, String notes){
+    public SafeLocation(String name, String address, String notes, String id){
         //this.id = id;
         this.name = name;
         this.address = address;
         this.notes = notes;
+        this.id = id;
     }
 
     public String getName() {
@@ -26,4 +29,25 @@ public class SafeLocation {
     public String getNotes() {
         return notes;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
+
