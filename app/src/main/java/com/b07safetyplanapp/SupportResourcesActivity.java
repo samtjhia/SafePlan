dd1883b9a3e19832d0a1171edbaa4e0f0eea5f0b
Annie Rong <annierong302@gmail.com>
2025-08-05 23:35:34 -0400

    feat: add back animation

--- app/src/main/java/com/b07safetyplanapp/SupportResourcesActivity.java
+++ app/src/main/java/com/b07safetyplanapp/SupportResourcesActivity.java
@@ -56,6 +56,12 @@ public class SupportResourcesActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void finish() { // back animation
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
     private void initializeFirebase() {
         database = FirebaseDatabase.getInstance("https://group8cscb07app-default-rtdb.firebaseio.com/");
 
